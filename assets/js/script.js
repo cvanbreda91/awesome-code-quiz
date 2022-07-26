414f683ae48c71bc69512e58b7ae7c853d4bd953
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-07-26 18:18:14 -0400

    Call showResult Function when timer reaches 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,118 +1,98 @@
 var timerEl = document.getElementById('timer-count');
 var seeMain = document.getElementsByClassName('hide');
-var question = document.getElementById('question');
-var A = document.getElementById('A');
-var B = document.getElementById('B');
-var C = document.getElementById('C');
-var D = document.getElementById('D');
-var questionNumber = 0;
 var playerScore = 0;
-
-
-document.getElementById('confirm').addEventListener("click", function clickCounter (){
-        questionNumber = questionNumber+1;
-    
-    }
-);
-document.getElementById('confirm').addEventListener("click", buildQuiz)
-
-function showResults(){
-    var seeMain = document.getElementById('main')
-    if(seeMain.style.visibility === "visible")
-    {
-    seeMain.style.visibility = "hidden";
-    }
-    else
-    {
-    seeMain.style.visibility = "visible";
-    }
-    window.alert ("You've completed the game you've scored " + playerScore + " points! - click 'OK' to see leaderboard");
-   };
-function keepingScore () {
-    playerScore=playerScore+1;
-};
-
-function buildQuiz (){
-    if (questionNumber===0) {
-        question.textContent = "question";
-        A.textContent = "A";
-        B.textContent = "B";
-        C.textContent = "C";
-        D.textContent = "D";
-        var rightAnswer = D;
-        if (rightAnswer.addEventListener("click", keepingScore)){}
-
-
-    }
-    else if (questionNumber===1) {
-        question.textContent = "How are you today?";
-        A.textContent = "meh";
-        B.textContent = "good";
-        C.textContent = "great";
-        D.textContent = "groovy";
-    }
-    else if (questionNumber === 2){
-        question.textContent = "Are you doing ok";
-        A.textContent = "yes";
-        B.textContent = "YES";
-        C.textContent = "Yes";
-        D.textContent = "mhmm";
-    }
-    else {
-        showResults()
-    }
-}
-
+var timerCount = 30;
+var questionNumber =-1;
+var seeMain = document.getElementById('main')
+var rightAnswer ="";
 
 function countdown() {
-    var timerCount = 30;
+    timerCount = 30;
   
     // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
     var timeInterval = setInterval(function () {
       // As long as the `timeLeft` is greater than 1
-      if (timerCount > 1) {
+      if (timerCount > 0) {
         timerEl.textContent = timerCount;
         // Decrement `timeCount` by 1
         timerCount--;
       }
       else {
-        // Once `timeCount` gets to 0
-        timerEl.textContent = 'Time is Up!';
-        // Use `clearInterval()` to stop the timer
+       showResults();
         clearInterval(timeInterval);
 
       }
     }, 1000);}
 
-    function unhideMain(){
-       var seeMain = document.getElementById('main')
-       if(seeMain.style.visibility === "hidden")
-       {
-       seeMain.style.visibility = "visible";
-       }
-       else
-       {
-       seeMain.style.visibility = "hidden";
-       }
-      };
 
-    function hideHeader(){
-        var hideTheHeader = document.getElementById('header')
-        if(hideTheHeader.style.visibility === "visible")
-        {
-            hideTheHeader.style.visibility = "hidden";
+
+var questionArray =[{
+    "question": "hey there",
+    "A": "OMG!",
+    "B": "IS THIS",
+    "C": "ACTUALLY",
+    "D": "WORKING?",
+    "rightAnswer": "C"},
+    {
+        "question": "holy cow",
+        "A": "I think",
+        "B": "this is",
+        "C": "really",
+        "D": "working!",
+        "rightAnswer": "D"},
+
+    {"question": "test",
+    "A": "test",
+    "B": "test",
+    "C": "test",
+    "D": "wtes!",
+    "rightAnswer": "A"}
+
+    ]
+
+
+    function buildQuiz() {
+
+        countQuestion();
+        if (questionNumber <3) {
+        question.textContent = questionArray[questionNumber]["question"], 
+        A.textContent = questionArray[questionNumber]["A"],
+        B.textContent = questionArray[questionNumber]["B"], 
+        C.textContent = questionArray[questionNumber]["C"], 
+        D.textContent = questionArray[questionNumber]["D"],
+        rightAnswer = questionArray[questionNumber]["rightAnswer"]}
+        else{
+        showResults();
+
+         }}
+    
+        function countQuestion(){
+            questionNumber = questionNumber+1;
         }
-        else
-        {
-            hideTheHeader.style.visibility = "visible";
-        }
-       };
+    
+
+// function buildQuiz (){
+    // rightAnswer = questionArray[i]["rightAnswer"];}}
+
+//if (rightAnswer){rightAnswer.addEventListener("click", keepingScore)}
+
  
 
-document.querySelector("#ready").addEventListener("click", unhideMain);
-document.querySelector("#ready").addEventListener("click", hideHeader);
-document.querySelector("#ready").addEventListener("click", countdown);
-document.querySelector("#ready").addEventListener("click", buildQuiz);
+function unhideMain(){
+    seeMain.style.visibility = "visible"
+    };
+
+function showResults(){
+    window.alert("game has ended");
+    seeMain.style.visibility = "hidden";
+    header.style.visibility = "hidden"
+}
+buildQuiz();
+document.querySelector("#ready").addEventListener("click", ()=> {unhideMain();countdown()});
+document.querySelector("#confirm").addEventListener("click",buildQuiz);
+document.querySelector("#skip").addEventListener("click",buildQuiz)
+
+
 
 
 
